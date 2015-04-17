3abe68f7ea7fad6b0413d4874680e8bc95ff92c0
Jonathan Nacionales <jnaciona@gmail.com>
2015-04-16 23:36:20 -0700

    Add more test cases
    I left several test cases unfilled. I have filled in some of those
    cases, but I also left a few unfilled, still.

--- test/SetContractTest.java
+++ test/SetContractTest.java
@@ -2,8 +2,14 @@ import java.util.Set;
 import org.junit.Test;
 import junit.framework.TestCase;
 
-abstract public class SetContractTest extends TestCase{
+abstract public class SetContractTest extends TestCase {
 
+	/** Get Instance of Subclass 
+	  *
+	  * Each subclass must return an instance of type Set. The tests within this
+	  * class will use this method to verify the runtime behavior of the
+	  * subclass.
+	  */
 	abstract public Set getSetInstance();
 
 	/** Enforce Empty Set has Zero Size 
@@ -28,7 +34,7 @@ abstract public class SetContractTest extends TestCase{
 		/// Assert
 		/// ======================================================================
 		///
-		assertEquals(set.size(),0);
+		assertEquals(set.size(), 0);
 	}
 
 	/** Enforce Empty Set Property
@@ -58,20 +64,83 @@ abstract public class SetContractTest extends TestCase{
 
 	/** Enforce Single Insertion Property
 	  *
+	  * When we insert an element into a set, we expect a cardinality of one
 	  */
 	public void test_set_has_size_one_when_inserting_an_item() {
+		/// ======================================================================
+		/// Arrange
+		/// ======================================================================
+		///
+		Set set = this.getSetInstance();
+
+		/// ======================================================================
+		/// Act
+		/// ======================================================================
+		/// I hard code an Integer into our set since it doesn't specifically
+		/// matter what I put in the set
+		///
+		set.add( new Integer(1) );
+
+		/// ======================================================================
+		/// Assert
+		/// ======================================================================
+		///
+		assertEquals( set.size(), 1 );
 	}
 
-	/** Enforce Single Insertion Property
+	/** Set Contains Item After Inserting It
 	  *
+	  * When we insert an item into a set, we expect that we can also retrieve it
+	  * later.
 	  */
 	public void test_set_contain_an_element_after_inserting_an_item() {
+		/// ======================================================================
+		/// Arrange
+		/// ======================================================================
+		///
+		Set set = this.getSetInstance();
+
+		/// ======================================================================
+		/// Act
+		/// ======================================================================
+		///
+		Integer number = new Integer(1);
+		set.add( number );
+		Boolean flag = set.contains( number );
+
+		/// ======================================================================
+		/// Assert
+		/// ======================================================================
+		///
+		assertTrue( flag );
 	}
 
 	/** Enforce Double Insertion Property
 	  *
+	  * When we insert the same item twice, we expect that the size of the set
+	  * only increments by one. 
 	  */
 	public void test_set_has_size_one_when_inserting_two_similar_items() {
+		/// ======================================================================
+		/// Arrange
+		/// ======================================================================
+		///
+		Set set = this.getSetInstance();
+
+		/// ======================================================================
+		/// Act
+		/// ======================================================================
+		///
+		Integer number1 =  new Number(1);
+		Integer number2 =  new Number(1);
+		set.add( number1 );
+		set.add( number2 );
+
+		/// ======================================================================
+		/// Assert
+		/// ======================================================================
+		///
+		assertEquals( set.size(), 1 );
 	}
 
 	/** Enforce Dissimmilar Insertion Property
@@ -80,10 +149,33 @@ abstract public class SetContractTest extends TestCase{
 	public void test_set_has_size_two_when_inserting_two_dissimmilar_items() {
 	}
 
-	/** Enforce Removal Item Property
-	  *
+	/** Remove an Item and Check the Size
+	  * 
+	  * When the caller removes an item from the set, the size of the set should
+	  * decriment by one.
 	  */
 	public void test_set_size_is_one_less_than_before_after_removing_element() {
+		/// ======================================================================
+		/// Arrange
+		/// ======================================================================
+		///
+		Set set = this.getSetInstance();
+
+		/// ======================================================================
+		/// Act
+		/// ======================================================================
+		///
+		Integer number1 =  new Number(1);
+		Integer number2 =  new Number(2);
+		set.add( number1 );
+		set.add( number2 );
+		set.remove( number1 );
+
+		/// ======================================================================
+		/// Assert
+		/// ======================================================================
+		///
+		assertEquals( set.size(), 1 );
 	}
 
 	/** Enforce Removal Item Property
